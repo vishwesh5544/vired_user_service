7914afcdece8c3c921398aa3e367adf6a9b949c8
vishwesh5544 <vishweshshukla20@gmail.com>
2024-09-23 12:51:05 +0530

    init

---
+++ src/main/kotlin/com/devops7/user_service/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.devops7.user_service
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(UserServiceApplication::class.java)
+    }
+
+}
